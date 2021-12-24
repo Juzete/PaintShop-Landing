f31299ca6a7a96fae437beea0c2d6f590b94e8a3
Maxim Korzenok <maximka00007777@gmail.com>
2021-12-24 03:44:13 +0300

    add new sections

--- js/swiper.js
+++ js/swiper.js
@@ -1,16 +1,13 @@
 import Swiper from "https://unpkg.com/swiper@7/swiper-bundle.esm.browser.min.js";
 
-const swiper = new Swiper(".swiper", {
-  // Optional parameters
+const swiperArrivals = new Swiper(".swiper", {
   direction: "horizontal",
   loop: true,
   slidesPerView: 4,
-  // Navigation arrows
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-  // And if we need scrollbar
   scrollbar: {
     el: ".swiper-scrollbar",
     draggable: true,
